9a69c347319a9c1db41f0365bfea1ea820e2f400
Eduardo <e_dornelles1992@hotmail.com>
2018-01-04 22:04:40 -0200

    criado primeiro endPoint de teste nas Controllers

--- src/main/java/br/com/eduardodornelles/infogoods/controller/UserController.java
+++ src/main/java/br/com/eduardodornelles/infogoods/controller/UserController.java
@@ -1,5 +1,25 @@
 package br.com.eduardodornelles.infogoods.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/usuarios")
+public class UserController extends AbstractController {
+
+	/**
+	 * RESTful method that get all prizes availables that can be redemeed by the
+	 * user *
+	 *
+	 * @return {@link ResponseEntity<HttpResponseDTO>}
+	 * @throws Exception
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public String Test()
+			throws Exception {
+		return "ACHOU";
+	}
 }
