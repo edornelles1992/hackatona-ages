c1298aea262395e31a805b72659044bc801362d5
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-06-10 22:50:44 -0300

    ajuste cors

--- src/main/java/hackatona/security/WebSecurity.java
+++ src/main/java/hackatona/security/WebSecurity.java
@@ -15,7 +15,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter implements Securit
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable().authorizeRequests()
+        http.cors().disable();
 //                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
 //                .anyRequest().authenticated()
 //                .and()
